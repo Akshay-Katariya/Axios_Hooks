c40b5a778c4661cd591b357864397f354a2cdd6c
Akshay Katariya <akshay_rajendra.katariya@syngenta.com>
2020-05-19 11:34:48 +0530

    Added global config for Axios

--- src/api/api.js
+++ src/api/api.js
@@ -8,6 +8,21 @@ export const API = axios.create({
 	baseURL: `http://jsonplaceholder.typicode.com/`,
 })
 
+let getToken = () => {
+	//return token from lcal storage
+	return 'f5af9f51-07e6-4332-8f1a-c0c11c1e3728'
+}
+
+//Global axios defaults
+API.defaults.headers.common['Authorization'] = `Bearer ${getToken()}`
+API.defaults.headers.post['Content-Type'] = 'application/json; charset=UTF-8'
+
+/**
+ * Override timeout default for the library
+ * Now all requests will wait 5 seconds before timing out
+ **/
+API.defaults.timeout = 5000
+
 // Add a request interceptor
 API.interceptors.request.use(
 	async (config) => {
--- src/api/apiInterface.js
+++ src/api/apiInterface.js
@@ -1,5 +1,19 @@
 import {API} from './api'
 
+/**
+ * Manage all API calls from here
+ * No need to add header explicitly its already managed via global config
+ * You can always override global config for individual request from here
+ **/
+
 export const getUserList = () => {
 	return API.get('users').catch((e) => console.log(e))
+}
+
+export const getUserPost = (userID) => {
+	return API.get(`users/${userID}/posts`).catch((e) => console.log(e))
+}
+
+export const postUserData = (requestBody) => {
+	API.post('posts', requestBody).catch((e) => console.log(e))
 }
